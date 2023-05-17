c2e51229b34e7e3fc363e30bf5ad865577cc9372
Kartik Jolapara <kjmickey003@gmail.com>
2023-05-18 01:16:36 +0530

    fix: make the HTTP method as PUT

--- src/templates/inventory.template.ts
+++ src/templates/inventory.template.ts
@@ -1,15 +1,15 @@
-import { ClientHttp } from "../http";
+import { ClientHttp } from '../http';
 export class InventoryTemplate extends ClientHttp {
   /**
    * @param {string} sku - sku is called stock keeping unit for inventory.
    * @param {object} payload - payload for inventory.
    */
-  async increaseInventory <Payload>(sku: string,payload:Payload):Promise<any> {
-    const response= await this.http.post(`/inventory/increment?sku=${sku}`,{...payload});
+  async increaseInventory<Payload>(sku: string, payload: Payload): Promise<any> {
+    const response = await this.http.put(`/inventory/increment?sku=${sku}`, { ...payload });
     return response.data;
   }
-  async decreaseInventory <Payload>(sku: string,payload:Payload):Promise<any> {
-    const response= await this.http.post(`/inventory/decrement?sku=${sku}`,{...payload});
+  async decreaseInventory<Payload>(sku: string, payload: Payload): Promise<any> {
+    const response = await this.http.put(`/inventory/decrement?sku=${sku}`, { ...payload });
     return response.data;
   }
 }
